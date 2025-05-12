43ada8f67e274ae194819f3e49b8e3e108fca911
s j <ramjane81819@gmail.com>
2025-05-13 00:41:21 +0530

    logget added

--- src/main/java/com/example/cacheExample/CacheExampleApplication.java
+++ src/main/java/com/example/cacheExample/CacheExampleApplication.java
@@ -1,15 +1,22 @@
 package com.example.cacheExample;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+
+import java.util.Locale;
 
 @SpringBootApplication
 @EnableCaching
 public class CacheExampleApplication {
 
+	private static final Logger log = LogManager.getLogger(CacheExampleApplication.class);
+
 	public static void main(String[] args) {
 		System.out.println("Testing.....");
+		log.info("welcome to main project...");
 		SpringApplication.run(CacheExampleApplication.class, args);
 	}
 
